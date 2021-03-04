8a4fb93e1137089b398cdfc4135fb40e95801084
firatmikyaz <66138781+firatmikyaz@users.noreply.github.com>
2021-03-05 00:00:17 +0300

    ReFactirng işlemi yaparak,kodlarımız daha düzenli oldu ve EntityFramework klasöürünü ekledik ve projemizi bir veri tabanına bağlamış olduk.

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,15 +1,21 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    //generic constraint : generic kısıt demek
+    //class : referans tip olabilir demek
+    //IEntity: IEntity olabilir veya IEntity imlplemente eden bir nesne olabilir.
+    //new: new'lenebilir olmalı.IEntity newlenemez.interface - soyut
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
-        List<T> GetAll();
+        List<T> GetAll(Expression<Func<T,bool>> filter = null); //Filte veredebilir vermeyedebilir.
+        T Get(Expression<Func<T, bool>> filter); //Filte zorunlu
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
-        List<T> GetAllByCategory(int categoryId);
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -36,10 +37,20 @@ namespace DataAccess.Concrete.InMemory
             _products.Remove(productToDelete);
         }
 
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Product> GetAll()
         {
             //Veri tabanını olduğu gbi döndürüyorum.Business iş katmanına döndürüyorum.
             return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Product> GetAllByCategory(int categoryId)
